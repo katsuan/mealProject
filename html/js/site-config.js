749875e92c5bc4ca9e929cbcb1ea062b7e3cda16
katsuan <mohaminn758@gmail.com>
2026-05-14 22:19:37 +0900

    [add]テスト用エンドポイントURLを設定

--- html/js/site-config.js
+++ html/js/site-config.js
@@ -1,4 +1,4 @@
 window.__MEAL_APP_CONFIG__ = Object.assign({}, window.__MEAL_APP_CONFIG__ || {}, {
   initialLiffId: '2010088825-CulmmaFc',
-  apiBaseUrl: ''
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbybPLi7hZ5WWIB2w2BZ_fUVNMb1FeMb8ow2ff15IjQ/dev'
 });
