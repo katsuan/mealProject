f3d57e3ad8d67c0c0fc7c2fc412c5c7289b45296
katsuan <mohaminn758@gmail.com>
2026-05-14 23:53:48 +0900

    [fix]スプレッドシート参照をID指定に統一

--- html/js/site-config.js
+++ html/js/site-config.js
@@ -1,4 +1,4 @@
 window.__MEAL_APP_CONFIG__ = Object.assign({}, window.__MEAL_APP_CONFIG__ || {}, {
   initialLiffId: '2010088825-CulmmaFc',
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyv_FQ4PmWYQAn7AZ3RevqW7t1mTzJktaczwdhsWbxRKeiLS8_7hCe1b8ChzPFXiR9z/exec'
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycby44a0PmVW3aaTUOw-hWEkOZQzCcvQqOTFAx4uchy5WcQK1K2WjkvCof90VK7ADSqIc/exec'
 });
